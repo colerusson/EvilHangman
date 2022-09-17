921340417c9a35c8bfbff51f1706fda83b8ecfba
Cole Russon <106703710+colerusson@users.noreply.github.com>
2022-09-16 18:06:24 -0600

    New Commit

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -1,9 +1,49 @@
 package hangman;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class EvilHangman {
+    public static void main(String[] args) throws EmptyDictionaryException, IOException, GuessAlreadyMadeException {
+        String fileName = args[0];
+        File fileInput = new File(fileName);
+        String inputWord = args[1];
+        EvilHangmanGame game = new EvilHangmanGame();
+        game.startGame(fileInput, inputWord.length());
+        int guesses = 10;
 
-    public static void main(String[] args) {
-
+        while (guesses > 0) {
+            System.out.println("You have " + guesses + " guesses left");
+            System.out.print("Used letters:");
+            for (char c : game.getGuessedLetters()) {
+                System.out.print(" ");
+                System.out.print(c);
+            }
+            System.out.print("\n");
+            System.out.print("Word: ");
+            System.out.println(game.getCurrentLetters());
+            System.out.print("Enter guess: ");
+            Scanner userGuess = new Scanner(System.in);
+            char letterGuess = userGuess.next().charAt(0);
+            System.out.print("\n");
+            if (game.getGuessedLetters().contains(letterGuess)) {
+                while (game.getGuessedLetters().contains(letterGuess)) {
+                    System.out.println("Letter already guessed, try again: ");
+                    letterGuess = userGuess.next().charAt(0);
+                }
+            }
+            int wordBefore = game.getLettersAdded();
+            game.makeGuess(Character.toLowerCase(letterGuess));
+            int wordAfter = game.getLettersAdded();
+            if (wordBefore == wordAfter) {
+                System.out.println("Sorry, there are no " + letterGuess +"'s");
+                --guesses;
+            }
+            else {
+                System.out.println("Yes, there is " + wordAfter + " " + letterGuess);
+            }
+        }
     }
 
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -2,22 +2,86 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
+    Set<String> allWords = new HashSet<>();
+    SortedSet<Character> guessedLetters = new TreeSet<>();
+    Map<String, Set<String>> allSets = new HashMap<>();
+    StringBuilder currentLetters = new StringBuilder();
+    int lettersAdded;
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
-
+        allWords.clear();
+        guessedLetters.clear();
+        Scanner scan = new Scanner(dictionary);
+        while (scan.hasNext()) {
+            String word = scan.next();
+            if (word.length() == wordLength) {
+                allWords.add(word.toLowerCase());
+            }
+        }
+        for (int i = 0; i < wordLength; ++i) {
+            currentLetters.setCharAt(i, '-');
+        }
     }
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        return null;
+        guessedLetters.add(guess);
+        for (String word : allWords) {
+            StringBuilder key = new StringBuilder();
+            for (int i = 0; i < word.length(); ++i) {
+                if (word.toLowerCase().charAt(i) == guess) {
+                    key.setCharAt(i, guess);
+                }
+                else {
+                    key.setCharAt(i,'-');
+                }
+            }
+            if (allSets.containsKey(key.toString())) {
+                allSets.get(key.toString()).add(word.toLowerCase());
+            }
+            else {
+                Set<String> newSet = new HashSet<>();
+                allSets.put(key.toString(), newSet);
+                allSets.get(key.toString()).add(word.toLowerCase());
+            }
+        }
+        int largestSet = 0;
+        String finalKey = "";
+        for (String name : allSets.keySet()) {
+            if (allSets.get(name).size() > largestSet) {
+                largestSet = allSets.get(name).size();
+                finalKey = name.toLowerCase();
+            }
+            if (allSets.get(name).size() == largestSet) {
+                // TODO: need implementation here to get the best set
+            }
+        }
+        allWords.clear();
+        allWords = allSets.get(finalKey);
+        lettersAdded = 0;
+        for (int i = 0; i < finalKey.length(); ++i) {
+            if (finalKey.charAt(i) != '-' && currentLetters.charAt(i) == '-') {
+                currentLetters.setCharAt(i, finalKey.charAt(i));
+                ++lettersAdded;
+            }
+        }
+
+        return allSets.get(finalKey);
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return null;
+        return guessedLetters;
+    }
+
+    public String getCurrentLetters() {
+        return currentLetters.toString();
+    }
+
+    public int getLettersAdded() {
+        return lettersAdded;
     }
 }
